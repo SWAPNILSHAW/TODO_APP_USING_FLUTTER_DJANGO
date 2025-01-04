24e6c0c8d0f0128c6170c567a703ecf2b9d27e3c
SWAPNIL SHAW <dancerswapnil@gmail.com>
2025-01-04 10:19:36 +0530

    FIRST COMMIT

--- TODO/new backend/backend/api/urls.py
+++
@@ -1,8 +0,0 @@
-from django.urls import path
-
-from .views import TodoList, TodoDetail
-
-urlpatterns = [
-    path('', TodoList.as_view(), name='todo_list'),
-    path('<int:pk>/', TodoDetail.as_view(), name='todo_detail'),
-]
